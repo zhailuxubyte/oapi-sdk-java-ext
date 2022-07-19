8b2134c6907d37a27571dbfe885d7265c0db85b9
zhailuxu <zhailuxu@bytedance.com>
2022-07-19 14:17:50 +0800

    add ext

---
+++ oapi-sdk-apache-httpclient/src/main/java/com/larksuite/oapi/sdk/apache/httpclient/ApacheHttpClientTransport.java
@@ -0,0 +1,124 @@
+package com.larksuite.oapi.sdk.apache.httpclient;
+
+import com.larksuite.oapi.core.httpclient.IHttpTransport;
+import com.larksuite.oapi.core.request.FormData;
+import com.larksuite.oapi.core.request.FormDataFile;
+import com.larksuite.oapi.core.request.RawRequest;
+import com.larksuite.oapi.core.response.RawResponse;
+import com.larksuite.oapi.core.utils.Jsons;
+import com.larksuite.oapi.core.utils.Strings;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class ApacheHttpClientTransport implements IHttpTransport {
+
+  private CloseableHttpClient httpclient;
+
+  private ApacheHttpClientTransport(Builder builder) {
+    httpclient = builder.httpclient;
+  }
+
+  public void Close() throws IOException {
+    httpclient.close();
+  }
+
+  public static Builder newBuilder() {
+    return new Builder();
+  }
+
+  @Override
+  public RawResponse execute(RawRequest rawRequest) throws Exception {
+    final String httpMethod = rawRequest.getHttpMethod();
+    // 创建请求对象request
+    HttpEntityEnclosingRequestBase request = new HttpEntityEnclosingRequestBase() {
+      @Override
+      public String getMethod() {
+        return httpMethod;
+      }
+    };
+
+    // 设置url，headers
+    request.setURI(URI.create(rawRequest.getReqUrl()));
+    for (Map.Entry<String, List<String>> entry : rawRequest.getHeaders().entrySet()) {
+      String key = entry.getKey();
+      for (String value : entry.getValue()) {
+        request.addHeader(key, value);
+      }
+    }
+
+    // 设置body
+    if (rawRequest.getBody() != null) {
+      Object body = rawRequest.getBody();
+      if (body instanceof FormData) {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+        builder.setContentType(ContentType.create(ContentType.MULTIPART_FORM_DATA.getMimeType()));
+        for (FormDataFile file : ((FormData) rawRequest.getBody()).getFiles()) {
+          builder.addBinaryBody(file.getFieldName(), file.getFile(),
+              ContentType.APPLICATION_OCTET_STREAM,
+              Strings.isEmpty(file.getFileName()) ? "unknown" : file.getFileName());
+        }
+        for (Map.Entry<String, Object> entry : ((FormData) rawRequest.getBody()).getParams()
+            .entrySet()) {
+          builder.addTextBody(entry.getKey(), (String) entry.getValue());
+        }
+        request.setEntity(builder.build());
+      } else {
+        StringEntity entity = new StringEntity(Jsons.LONG_TO_STR_GSON.toJson(rawRequest.getBody()));
+        request.setEntity(entity);
+      }
+    }
+
+    // 发起调用
+    CloseableHttpResponse response = httpclient.execute(request);
+
+    // 转换结果为通用结果
+    byte[] result = EntityUtils.toByteArray(response.getEntity());
+    RawResponse rawResponse = new RawResponse();
+    rawResponse.setStatusCode(response.getStatusLine().getStatusCode());
+    rawResponse.setBody(result);
+    rawResponse.setContentType(rawResponse.getContentType());
+    Map<String, List<String>> headers = new HashMap<>();
+    for (Header header : response.getAllHeaders()) {
+      if (headers.containsKey(header.getName())) {
+        headers.get(header.getName()).add(header.getValue());
+      } else {
+        List<String> values = new ArrayList<>();
+        values.add(header.getValue());
+        headers.put(header.getName(), values);
+      }
+    }
+    rawResponse.setHeaders(headers);
+    return rawResponse;
+  }
+
+  public static final class Builder {
+
+    private CloseableHttpClient httpclient;
+
+    private Builder() {
+    }
+
+    public Builder httpclient(CloseableHttpClient httpclient) {
+      this.httpclient = httpclient;
+      return this;
+    }
+
+    public ApacheHttpClientTransport build() {
+      return new ApacheHttpClientTransport(this);
+    }
+  }
+}
---
+++ oapi-sdk-servlet-ext/src/main/java/com/larksuite/oapi/sdk/servlet/ext/ServletAdapter.java
@@ -0,0 +1,56 @@
+package com.larksuite.oapi.sdk.servlet.ext;
+
+import com.lark.oapi.card.CardActionHandler;
+import com.lark.oapi.core.request.EventReq;
+import com.lark.oapi.core.response.EventResp;
+import com.lark.oapi.event.EventDispatcher;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet的适配器，用于适配基于Servlet技术栈实现的Web服务
+ */
+@Resource
+public class ServletAdapter {
+    private static final HttpTranslator HTTP_TRANSLATOR = new HttpTranslator();
+
+    /**
+     * 处理消息事件
+     *
+     * @param req
+     * @param response
+     * @param eventDispatcher
+     * @throws Throwable
+     */
+    public void handleEvent(HttpServletRequest req, HttpServletResponse response, EventDispatcher eventDispatcher) throws Throwable {
+        // 转换请求对象
+        EventReq eventReq = HTTP_TRANSLATOR.translate(req);
+
+        // 处理请求
+        EventResp resp = eventDispatcher.handle(eventReq);
+
+        // 回写结果
+        HTTP_TRANSLATOR.write(response, resp);
+    }
+
+    /**
+     * 处理卡片消息
+     *
+     * @param req
+     * @param response
+     * @param handler
+     * @throws Throwable
+     */
+    public void handleCardAction(HttpServletRequest req, HttpServletResponse response, CardActionHandler handler) throws Throwable {
+        // 转换请求对象
+        EventReq eventReq = HTTP_TRANSLATOR.translate(req);
+
+        // 处理请求
+        EventResp resp = handler.handle(eventReq);
+
+        // 回写结果
+        HTTP_TRANSLATOR.write(response, resp);
+    }
+}
